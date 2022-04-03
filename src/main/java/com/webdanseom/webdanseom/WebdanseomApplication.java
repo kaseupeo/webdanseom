1e6aab094521729cf6e6d1b225fc8149bfd5eae3
donghyun <donghyun3794@naver.com>
2022-04-04 02:05:58 +0900

    test

---
+++ src/main/java/com/webdanseom/webdanseom/WebdanseomApplication.java
@@ -0,0 +1,13 @@
+package com.webdanseom.webdanseom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdanseomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdanseomApplication.class, args);
+	}
+
+}
