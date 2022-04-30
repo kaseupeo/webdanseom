5c49baf4e5fabc03b6fa7ffecd31be64e4f538c3
donghyun <donghyun3794@naver.com>
2022-05-01 01:03:16 +0900

    feat: Repository 추가
    NurseRepository.java, NurseGroupRepository.java

---
+++ src/main/java/com/webdanseom/nurseonduty/repo/NurseGroupRepository.java
@@ -0,0 +1,7 @@
+package com.webdanseom.nurseonduty.repo;
+
+import com.webdanseom.nurseonduty.model.NurseGroup;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NurseGroupRepository extends CrudRepository<NurseGroup, Long> {
+}
---
+++ src/main/java/com/webdanseom/nurseonduty/repo/NurseRepository.java
@@ -0,0 +1,7 @@
+package com.webdanseom.nurseonduty.repo;
+
+import com.webdanseom.nurseonduty.model.Nurse;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NurseRepository extends CrudRepository<Nurse, Long> {
+}
