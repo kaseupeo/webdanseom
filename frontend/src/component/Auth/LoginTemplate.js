35d4deaec78acc1d2aa609a22be5727f9c09d4db
Jinwook Jeong <95017248+JinwookJeong030@users.noreply.github.com>
2022-05-05 17:46:30 +0900

    로그인 레이아웃 수정

--- frontend/src/component/Auth/LoginTemplate.js
+++ frontend/src/component/Auth/LoginTemplate.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './LoginElement.scss';
-import './AuthTemplate.scss'
+import './AuthTemplate.scss';
 import { Link } from 'react-router-dom';
-const LoginTemplate = () => {
-    return (
 
-            <div className="AuthTemplate">
-            <div className="login-title">로그인</div>
-                <div className="content">
-             <div className="LoginElement">
-             <input type="text" placeholder="아이디"/>
-             <input type="password" placeholder="비밀번호"/>
-             <button className="loginBtn" type="submit">로그인</button>
-             <hr/>
-            <div className="otherElement">
-             <Link to="/auth/findPW" className="findPasswordBtn">계정 찾기</Link>
-             <Link to="/auth/signup" className="signBtn">회원가입</Link>
-            </div>
+const LoginTemplate = () => {
+  return (
+    <div className="AuthTemplate">
+      <div className="login-title">로그인</div>
+      <div className="content">
+        <div className="LoginElement">
+          <input type="text" placeholder="아이디" />
+          <input type="password" placeholder="비밀번호" />
+          <button className="loginBtn" type="submit">
+            로그인
+          </button>
+          <hr />
+          <div className="otherElement">
+            <Link to="/auth/findPW" className="findPasswordBtn">
+              계정 찾기
+            </Link>
+            <Link to="/auth/signup" className="signBtn">
+              회원가입
+            </Link>
+          </div>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LoginTemplate;
--- frontend/src/component/Auth/SignUpTemplate.js
+++ frontend/src/component/Auth/SignUpTemplate.js
@@ -1,42 +1,35 @@
 import React from 'react';
 import './SignUpElement.scss';
-import './AuthTemplate.scss'
+import './AuthTemplate.scss';
 import { Link } from 'react-router-dom';
 const JoinElement = () => {
+  /*회원 가입시 처리*/
+  const onClickSignUp = () => {
+    alert('회원가입이 완료되었습니다!');
+  };
 
+  return (
+    <div className="AuthTemplate">
+      <div className="login-title">회원가입</div>
 
-
-    /*회원 가입시 처리*/
-    const onClickSignUp = () => {
-     alert("회원가입이 완료되었습니다!");
-    };
-
-    return (
-         <div className="AuthTemplate">
-
-
-
-                <div className="login-title">회원가입</div>
-
-                <div className="content">
-
-
-
+      <div className="content">
         <div className="SignUpElement">
-            <b>이메일
-            <input type="text" placeholder="이메일"/>
-            </b>
-            <b>비밀번호</b>
-            <input type="password" placeholder="비밀번호"/>
-            <input type="password" placeholder="비밀번호 재확인"/>
-            <b>이름</b>
-            <input type="text" placeholder="이름"/>
-            <Link to="/auth/login"  className="signBtn">가입하기</Link>
-
+          <b>
+            이메일
+            <input type="text" placeholder="이메일" />
+          </b>
+          <b>비밀번호</b>
+          <input type="password" placeholder="비밀번호" />
+          <input type="password" placeholder="비밀번호 재확인" />
+          <b>이름</b>
+          <input type="text" placeholder="이름" />
+          <Link to="/auth/login" className="signBtn">
+            가입하기
+          </Link>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default JoinElement;
