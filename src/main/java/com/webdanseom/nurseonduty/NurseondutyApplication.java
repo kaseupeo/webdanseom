45df946e2b91d059cc027d057bfc6594f5c5ef90
donghyun <donghyun3794@naver.com>
2022-05-08 21:24:31 +0900

    feat: OauthController 추가
    소셜 로그인 컨트롤러

--- src/main/java/com/webdanseom/nurseonduty/NurseondutyApplication.java
+++ src/main/java/com/webdanseom/nurseonduty/NurseondutyApplication.java
@@ -2,6 +2,8 @@ package com.webdanseom.nurseonduty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class NurseondutyApplication {
@@ -9,5 +11,4 @@ public class NurseondutyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NurseondutyApplication.class, args);
 	}
-
 }
--- src/main/java/com/webdanseom/nurseonduty/controller/OauthController.java
+++ src/main/java/com/webdanseom/nurseonduty/controller/OauthController.java
@@ -1,4 +1,67 @@
 package com.webdanseom.nurseonduty.controller;
 
+import com.webdanseom.nurseonduty.jwt.JwtUtil;
+import com.webdanseom.nurseonduty.model.Member;
+import com.webdanseom.nurseonduty.model.Response;
+import com.webdanseom.nurseonduty.model.requset.RequestSocialData;
+import com.webdanseom.nurseonduty.service.AuthService;
+import com.webdanseom.nurseonduty.service.CookieUtil;
+import com.webdanseom.nurseonduty.service.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/oauth")
 public class OauthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private CookieUtil cookieUtil;
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @PostMapping("/signup/naver")
+    public Response signUpNaverUser(
+            @RequestBody RequestSocialData socialData
+            ) {
+        Response response;
+        try {
+            authService.signUpSocialUser(socialData);
+            response = new Response("success", "성공적으로 회원가입을 완료했습니다.", null);
+        } catch (Exception e) {
+            response = new Response("error", "회원가입 실패했습니다.", e.getMessage());
+        }
+        return response;
+    }
+
+    @GetMapping("/login/naver")
+    public Response loginNaverUser(
+            @RequestBody RequestSocialData socialData, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse
+    ) {
+        Response response;
+        try {
+            final Member member = authService.loginSocialUser(socialData.getEmail(), socialData.getType());
+            final String token = jwtUtil.generateToken(member);
+            final String refreshJwt = jwtUtil.generateRefreshToken(member);
+            Cookie accessToken = cookieUtil.createCookie(JwtUtil.ACCESS_TOKEN_NAME, token);
+            Cookie refreshToken = cookieUtil.createCookie(JwtUtil.REFRESH_TOKEN_NAME, refreshJwt);
+            redisUtil.setDataExpire(refreshJwt, member.getEmail(), JwtUtil.REFRESH_TOKEN_VALIDATION_SECOND);
+            httpServletResponse.addCookie(accessToken);
+            httpServletResponse.addCookie(refreshToken);
+            response = new Response("success", "로그인에 성공했습니다.", token);
+        } catch (Exception e) {
+            response = new Response("error", "로그인에 실패했습니다.", e.getMessage());
+        }
+        return response;
+    }
 }
