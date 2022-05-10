14f4a428867051d78e52361e7c32c174c9faf56c
pyoyeongun <pyoyeongun@gmail.com>
2022-05-10 15:41:27 +0900

    Changes

--- src/main/java/com/webdanseom/nurseonduty/service/AuthService.java
+++ src/main/java/com/webdanseom/nurseonduty/service/AuthService.java
@@ -8,25 +8,36 @@ import javassist.NotFoundException;
 public interface AuthService {
     final String REDIS_CHANGE_PASSWORD_PREFIX="CPW";
 
+    //일반 회원가입
     void signUpUser(Member member);
 
+    //소셜 회원가입
     void signUpSocialUser(RequestSocialData member);
 
+    //소셜 로그인
     Member loginSocialUser(String id, String type) throws NotFoundException;
 
+    //일반 로그인
     Member loginUser(String email, String password) throws Exception;
 
+    //이메일 인증
     Member findByEmail(String email) throws NotFoundException;
 
+    //인증본호 이메일 받기
     void verifyEmail(String key) throws NotFoundException;
 
+    //인증번호 이메일 보내기
     void sendVerificationMail(Member member) throws NotFoundException;
 
+    //로그인을 위한 유저정보 수정
     void modifyUserRole(Member member, UserRole userRole);
 
+    //이메일에서 보낸 인증번호 토큰
     boolean isPasswordUuidValidate(String key);
 
+    //비밀번호 변경
     void changePassword(Member member, String password) throws NotFoundException;
 
+    //비밀번호 변경요청
     void requestChangePassword(Member member) throws NotFoundException;
 }
--- src/test/java/com/webdanseom/nurseonduty/repo/MemberRepositoryUnitTest.java
+++ src/test/java/com/webdanseom/nurseonduty/repo/MemberRepositoryUnitTest.java
@@ -1,4 +1,15 @@
 package com.webdanseom.nurseonduty.repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@DataJpaTest
 public class MemberRepositoryUnitTest {
+    @Autowired
+    private MemberRepository memberRepository;
 }
