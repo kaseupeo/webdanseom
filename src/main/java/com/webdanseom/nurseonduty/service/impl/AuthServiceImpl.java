2924d49502be35c5b40f433633749d1b71642c86
donghyun <donghyun3794@naver.com>
2022-05-10 15:43:13 +0900

    feat: slf4j 의존성 추가

--- src/main/java/com/webdanseom/nurseonduty/service/impl/AuthServiceImpl.java
+++ src/main/java/com/webdanseom/nurseonduty/service/impl/AuthServiceImpl.java
@@ -130,7 +130,7 @@ public class AuthServiceImpl implements AuthService {
      */
     @Override
     public void sendVerificationMail(Member member) throws NotFoundException {
-        String VERIFICATION_LINK = "http://localhost:8080/user/verify/";
+        String VERIFICATION_LINK = "http://localhost:8080/member/verify/";
         if (member == null) throw new NotFoundException("멤버가 조회되지 않습니다.");
         UUID uuid = UUID.randomUUID();
         redisUtil.setDataExpire(uuid.toString(), member.getEmail(), 1800L);
@@ -166,7 +166,7 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public void requestChangePassword(Member member) throws NotFoundException {
-        String CHANGE_PASSWORD_LINK = "http://localhost:8080/user/password/";
+        String CHANGE_PASSWORD_LINK = "http://localhost:8080/member/password/";
         if (member == null) throw new NotFoundException("멤버가 조회되지 않습니다.");
         String key = REDIS_CHANGE_PASSWORD_PREFIX + UUID.randomUUID();
         redisUtil.setDataExpire(key, member.getEmail(), 1800L);
