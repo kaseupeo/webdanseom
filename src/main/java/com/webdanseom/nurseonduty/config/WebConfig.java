f8e8de32ac6cf920b03c571c875fcc282bfa20a7
pyoyeongun <pyoyeongun@gmail.com>
2022-05-15 23:27:18 +0900

    docs : 주석 수정

--- src/main/java/com/webdanseom/nurseonduty/config/WebConfig.java
+++ src/main/java/com/webdanseom/nurseonduty/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.webdanseom.nurseonduty.config;
-
+/**
+ * front랑 연동할 때 쓰는 뼈대
+ * */
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
