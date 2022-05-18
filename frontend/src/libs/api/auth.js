4ab14c0ddbd4b2b519ad54abd6c93ec15e2db518
Jinwook Jeong <95017248+JinwookJeong030@users.noreply.github.com>
2022-05-19 01:07:01 +0900

    fix(frontend): 비밀번호 찾기 수정(미완)
    비밀번호 찾기 리덕스방식으로 수정

--- frontend/src/libs/api/auth.js
+++ frontend/src/libs/api/auth.js
@@ -5,6 +5,12 @@
  */
 import client from './client';
 
+/**
+ *  로그인 post 통신
+ *
+ * @param {*} param0
+ * @returns {response: "성공여부" , message: "메시지", data: "token" or null }
+ */
 export const login = ({ email, password }) =>
   client
     .post('/api/member/login', {
@@ -15,7 +21,11 @@ export const login = ({ email, password }) =>
       console.log(response.data);
       return response;
     });
-
+/**
+ *  회원가입 post 통신
+ * @param {*} param0
+ * @returns  {response: "성공여부" , message: "메시지", data: "메시지" or null }
+ */
 export const signup = ({ email, password, name, phoneNumber }) =>
   client
     .post('/api/member/signup', {
@@ -28,11 +38,18 @@ export const signup = ({ email, password, name, phoneNumber }) =>
       console.log(response.data);
       return response;
     });
-
-export const verify = ({ userId }) => client.getUser('/verify', { userId });
-export const getUser = (userId) =>
-  client.get(`/api/member/${userId}/getUser`, {
-    headers: {
-      Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token')),
-    },
-  });
+/**
+ * 비밀번호 찾기
+ * @param {} param0
+ * @returns
+ */
+export const findPassword = ({ email }) =>
+  client.post(
+    '/api/member/verify',
+    {
+      email,
+    }.then((response) => {
+      console.log(response.data);
+      return response;
+    }),
+  );
