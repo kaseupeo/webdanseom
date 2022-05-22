177d46e160bae262e8f8c1955e4d251034ce6724
pyoyeongun <pyoyeongun@gmail.com>
2022-05-22 21:06:50 +0900

    chore : @Service 추가

--- src/main/java/com/webdanseom/nurseonduty/service/impl/DutyServiceImpl.java
+++ src/main/java/com/webdanseom/nurseonduty/service/impl/DutyServiceImpl.java
@@ -1,7 +1,9 @@
 package com.webdanseom.nurseonduty.service.impl;
 
 import com.webdanseom.nurseonduty.service.DutyService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DutyServiceImpl implements DutyService {
     //듀티추가
     //초기화
