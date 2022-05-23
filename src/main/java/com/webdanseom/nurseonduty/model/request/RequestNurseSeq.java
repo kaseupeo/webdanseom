80b99428671fcd51a3c297755a2434b9dbba071c
donghyun <donghyun3794@naver.com>
2022-05-23 17:20:25 +0900

    feat: 간호사 삭제 기능 추가

---
+++ src/main/java/com/webdanseom/nurseonduty/model/request/RequestNurseSeq.java
@@ -0,0 +1,19 @@
+package com.webdanseom.nurseonduty.model.request;
+/**
+ * 파일명: RequestNurseSeq.java
+ * 설명: request
+ * 작성일자:2022.05.23
+ * 작성자:신동현
+ * 수정일자:
+ * 수정자:
+ */
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RequestNurseSeq {
+    private int seq;
+}
