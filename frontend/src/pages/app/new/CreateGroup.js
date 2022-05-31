2b425e732d4a834561cdb9130db956cedcae3d27
Jinwook Jeong <95017248+JinwookJeong030@users.noreply.github.com>
2022-05-31 16:22:08 +0900

    feat(frontend): 로그인 후 그룹생성 메뉴 추가
    로그인 후 그룹생성 메뉴 준비

---
+++ frontend/src/pages/app/new/CreateGroup.js
@@ -0,0 +1,13 @@
+import Title from '../../../components/app/common/Title';
+
+const ManagementWork = () => {
+  return (
+    <div>
+      <span>
+        <Title title="그룹 생성" />
+      </span>
+    </div>
+  );
+};
+
+export default ManagementWork;
---
+++ frontend/src/pages/app/new/JoinGroup.js
@@ -0,0 +1,13 @@
+import Title from '../../../components/app/common/Title';
+
+const JoinGroup = () => {
+  return (
+    <div>
+      <span>
+        <Title title="그룹 참가" />
+      </span>
+    </div>
+  );
+};
+
+export default JoinGroup;
